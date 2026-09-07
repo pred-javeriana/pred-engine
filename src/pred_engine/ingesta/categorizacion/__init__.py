d2f26d0d2b68f5dc2054d364fbcc4c2bfceb116a
TomasPinila <tomaspinilla0508@gmail.com>
2026-09-06 20:34:27 -0500

    feat: calcular CV2 sin estado sobre demandas estrictamente positivas

--- src/pred_engine/ingesta/categorizacion/__init__.py
+++ src/pred_engine/ingesta/categorizacion/__init__.py
@@ -1,6 +1,7 @@
 """Motor de topologia Syntetos-Boylan: ADI, CV² y enrutado de sku_class."""
 
 from pred_engine.ingesta.categorizacion.adi import compute_adi
+from pred_engine.ingesta.categorizacion.cv2 import compute_cv2
 from pred_engine.ingesta.categorizacion.errores import (
     TopologyContractError,
     TopologyMathError,
@@ -12,4 +13,5 @@ __all__ = [
     "TopologyMathError",
     "TopologyRoutingError",
     "compute_adi",
+    "compute_cv2",
 ]
